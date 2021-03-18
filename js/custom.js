c5e2700ce2858b5238e630c1f0a47c08ce9476ce
NgoKimHoangMinh <hoangminhcp10@gmail.com>
2021-03-18 18:29:52 +0700

    Complete

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+jQuery(function ($) {
+
+    $(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+}); 
+});
+
+///hambuger
+var menu=document.getElementById('hambuger_menu');
+var btnhambuger=document.getElementById('button-event');
+function show()
+{
+  btnhambuger.classList.toggle('change_btn');
+  menu.classList.toggle("show_menu");
+}
